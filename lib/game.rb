0e72792fee285e09b61725ac9992155ab1caad17
Reflection <reflectionoflines@gmail.com>
2023-01-19 16:20:29 -0600

    Create a method that can give the breaker feedback on their guess.

--- lib/game.rb
+++ lib/game.rb
@@ -8,16 +8,19 @@ class Game
 
   def start_game
     @turns_left = 15
+    @feedback = Array.new(4, "")
     recieve_code
     compare_codes
   end
 
   def recieve_code
     @code = @coder.create_code
+    print @code
   end
 
   def recieve_guess
     @guess = @breaker.guess_code
+    @guess = @guess.map { |num| num.to_i }
   end
 
   def compare_codes
@@ -26,8 +29,18 @@ class Game
       puts "Correct!"
     else
       @turns_left -= 1
-      puts "Try again. #{@turns_left} tries left!"
+      print give_feedback(@code, @guess)
     end
     compare_codes
+  end
+
+  def give_feedback(code, guess)
+    code.map.with_index do |num, idx|
+      if @feedback[idx].instance_of? String
+        @feedback[idx] = (guess.include?(num) ? "O" : "X")
+        @feedback[idx] = num if num == guess[idx]
+      end
+    end
+    return @feedback
   end
 end
